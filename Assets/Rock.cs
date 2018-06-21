7495875f2ac16520f915d9f0d2a554547872922a
Ronan <ronanpsf@gmail.com>
2018-06-21 09:26:40 +0200

    explosion, need an import of prefab

---
+++ Assets/Rock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+
+    public GameObject particles;
+
+    public string collisionObjectName;
+    // Use this for initialization
+    void Start () {
+        Destroy(gameObject, 15);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == collisionObjectName)
+        {
+            Destroy(collision.gameObject);
+            GameObject g = Instantiate(particles, collision.contacts[0].point, Quaternion.identity);
+            Destroy(g, 4);
+        }
+            
+    }
+}
