b505a667ae1a977f793e12dca6543e487eedf4e6
Ronan <ronanpsf@gmail.com>
2018-06-21 17:36:40 +0200

    energy bar

---
+++ Assets/powerRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class powerRenderer : MonoBehaviour {
+
+    float maxEnergy = 100;
+    Image fillImg;
+    float energy;
+	// Use this for initialization
+	void Start () {
+        fillImg = this.GetComponent<Image>();
+        energy = this.GetComponentInParent<HandsData>().CurrentEnergy;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(energy > 0)
+        {
+            fillImg.fillAmount = energy / maxEnergy;
+        }
+	}
+}
