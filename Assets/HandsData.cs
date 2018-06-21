d202a4110eb76bd33f6b30acf373df11e8e622ce
Timothé <noki.dev@gmail.com>
2018-06-21 10:46:56 +0200

    WIP = Puller effect, hands data for energy

---
+++ Assets/HandsData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandsData : MonoBehaviour {
+
+    public float maxEnergy = 100;
+
+    public delegate void FloatEmitter(float value);
+    public delegate void BoolEmitter(bool value);
+    public delegate void IntegerEmitter(int value);
+
+
+
+    public event FloatEmitter energyChanged;
+
+    public float CurrentEnergy
+    {
+        get
+        {
+            return currentEnergy;
+        }
+
+        set
+        { 
+            currentEnergy = value;
+            if(currentEnergy < 0) currentEnergy = 0;
+            if(currentEnergy > maxEnergy) currentEnergy = maxEnergy;
+
+            if(energyChanged != null) energyChanged(currentEnergy);
+        }
+    }
+    private float currentEnergy; // Energy percent; 
+
+    // Use this for initialization
+    void Start () {
+        CurrentEnergy = maxEnergy;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Puller.cs
+++ Assets/Puller.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Puller : MonoBehaviour {
 
-    public Vector3 originForce; //Force on origin. Decrease over distance.
-    public Vector3 maxPullDistance; // Max distance in which the objects can be pulled.
+    public Vector3 maxPullForce; //Force on origin. Decrease over distance.
+    public float maxPullDistance = 10f; // Max distance in which the objects can be pulled.
+
+    // Use this for initialization
+
+    private bool started = true;
+    private List<Rigidbody> pulledObjects = new List<Rigidbody>();
+    private List<Rigidbody> catchedObjects = new List<Rigidbody>();
+
+    public float catchDistance = 0.5f;
 
 
-	// Use this for initialization
 	void Start () {
 		
 	}
@@ -18,14 +25,53 @@ public class Puller : MonoBehaviour {
 
     }
 
+    private void FixedUpdate()
+    {
+        if (started)
+        {
+            List<Rigidbody> tmp = new List<Rigidbody>();
+            foreach(var pulledObject in pulledObjects)
+            {
+                Vector3 direction = (transform.position - pulledObject.transform.position);
+                float distance = direction.magnitude;
+                if(distance < catchDistance)
+                {
+                    pulledObject.constraints = RigidbodyConstraints.FreezePosition;
+                    catchedObjects.Add(pulledObject);
+                }
+                else
+                {
+                    tmp.Add(pulledObject);
+                    Vector3 appliedForce = Vector3.Lerp(maxPullForce, Vector3.zero, distance/maxPullDistance); // Keep objects at close range.
+                    pulledObject.AddForceAtPosition(appliedForce, transform.position);
+                }
+            }
+            pulledObjects = tmp;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        pulledObjects.Add(other.GetComponent<Rigidbody>());
+    }
+
     public void Pull()
     {
+        started = true;
         //Pull effect code.
     }
 
     public void Stop()
     {
+        started = false;
+        pulledObjects.Clear();
+        foreach(var catchedObject in catchedObjects)
+        {
+            catchedObject.constraints = RigidbodyConstraints.None;
+        }
         //Stop pull effect nicely.
         //Stop animation.
     }
+
+
 }
