5d0141737bd054e148d13ec4401b508c0096d515
Léo <leo.massonnat@hotmail.fr>
2018-06-22 13:12:24 +0200

    dispawn rock

--- Assets/TargetHit.cs
+++ Assets/TargetHit.cs
@@ -8,8 +8,6 @@ public class TargetHit : MonoBehaviour {
 
     public AudioClip crashSoft;
 
-    //public Score score;
-
     private AudioSource source;
     private float lowPitchRange = .75F;
     private float highPitchRange = 1.5F;
@@ -29,16 +27,10 @@ public class TargetHit : MonoBehaviour {
     {
 
     }
-
-    void Awake()
-    {
-        
-    }
-
-
+    
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == collisionObjectName)
+        if (collision.gameObject.tag == collisionObjectName)
         {
             Destroy(collision.gameObject);
             GameObject g = Instantiate(particles, collision.contacts[0].point, Quaternion.identity);
@@ -52,5 +44,21 @@ public class TargetHit : MonoBehaviour {
             source.PlayOneShot(crashSoft, hitVol);
         }
 
+        if (collision.gameObject.name == "Terrain")
+        {
+            Vector3 pos = gameObject.transform.position;
+            GameObject mainCamera = GameObject.FindGameObjectsWithTag("MainCamera")[0];
+            float max;
+            if (mainCamera.GetComponents<Spawn>()[0].nameTag == "Rock")
+            {
+                max = mainCamera.GetComponents<Spawn>()[0].maxDistance;
+            } else
+            {
+                max = mainCamera.GetComponents<Spawn>()[1].maxDistance;
+            }
+            Debug.Log("dist" + max);
+            if (pos.x > max || pos.z > max || pos.x < 0 - max || pos.z < 0 - max)
+                Destroy(gameObject);
+        }
     }
 }
